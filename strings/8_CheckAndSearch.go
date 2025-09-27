2829b70b04993b5d6bf44674e53dbeebd10f7c55
Usynin Sergei <usynin.s00@mail.ru>
2025-09-27 17:40:09 +0300

    Add Chech and lang.

---
+++ strings/8_CheckAndSearch.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "golang is cool"
+
+	fmt.Println(strings.Contains(s, "go")) // true
+	fmt.Println(strings.HasPrefix(s, "go")) // true
+	fmt.Println(strings.HasSuffix(s, "cool")) // true
+	fmt.Println(strings.Index(s, "lang")) // 2 (позиция в байтах)
+}
