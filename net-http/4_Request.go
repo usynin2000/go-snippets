fc9ca650313044ad62eec4535fe1b3c96b775239
Usynin Sergei <usynin.s00@mail.ru>
2025-10-07 19:53:43 +0300

    Add fix request.

--- net-http/4_Request.go
+++ net-http/4_Request.go
@@ -5,18 +5,47 @@ import (
 	"net/http"
 )
 
-func mainPage(res http.ResponseWriter, req *http.Request) {
+func reqUrlQuery(res http.ResponseWriter, req *http.Request) {
 	body := fmt.Sprintf("Method: %s\r\n", req.Method)
 	body += "Header =================\r\n"
 	for k, v := range req.Header {
 		body += fmt.Sprintf("%s: %v\r\n", k, v)
 	}
+	
+	body += "Query parameters =========\r\n"
+	for k, v := range req.URL.Query() {
+		body += fmt.Sprintf("%s: %v\r\n", k, v)
+	}
+	
 	res.Write([]byte(body))
+}
+
+func parseFormPage(res http.ResponseWriter, req *http.Request) {
+    body := fmt.Sprintf("Method: %s\r\n", req.Method)
+    body += "Header ===============\r\n"
+    for k, v := range req.Header {
+        body += fmt.Sprintf("%s: %v\r\n", k, v)
+    }
+    body += "Query parameters ===============\r\n"
+    if err := req.ParseForm(); err != nil {
+        res.Write([]byte(err.Error()))
+        return
+    }
+    for k, v := range req.Form {
+        body += fmt.Sprintf("%s: %v\r\n", k, v)
+    }
+    res.Write([]byte(body))
+}
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("/"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/parse", parseFormPage)
+	mux.HandleFunc("/query", reqUrlQuery)
 	mux.HandleFunc("/", mainPage)
 
 	err := http.ListenAndServe(":8080", mux)
@@ -26,3 +55,21 @@ func main() {
 }
 
 // запусти http://localhost:8080/?id=12345&name=John%20Doe&filter=town&filter=country.
+
+
+// req.URL.Query()
+// Берёт параметры только из URL (часть после ?).
+
+// GET /query?name=Tom&age=20 → будут распарсены name, age.
+
+
+// req.ParseForm()
+// объединяет данные из query string и тела формы (
+// если Content-Type подходит: application/x-www-form-urlencoded и POST.
+// req.Form = URL.Query() + тело запроса.
+
+// curl --location 'http://localhost:8080/parse?test_2=info2' \
+// --header 'Content-Type: application/x-www-form-urlencoded' \
+// --data-urlencode 'test=info'
+
+//  Вот так например будет и квери и body через urlencode принимать
