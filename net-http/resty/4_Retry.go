2996921fe996b8ec8374f764c326d56a7ce5527d
Usynin Sergei <usynin.s00@mail.ru>
2025-10-09 21:30:41 +0300

    Add retry with resty.

--- net-http/resty/4_Retry.go
+++ net-http/resty/4_Retry.go
@@ -1,1 +1,46 @@
-p
+package main
+
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"time"
+)
+
+func main() {
+	client := resty.New()
+
+
+	client.
+		// устанавливаем количестов повторений
+		SetRetryCount(3).
+		// длительность ожидания между попытками
+		SetRetryWaitTime(30 * time.Second).
+		// длительность максимального ожидания
+		SetRetryMaxWaitTime(90 * time.Second)
+
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(`{"title": "foo", "body": "bar", "userId": 7}`).
+		Post("https://jsonplaceholder.typicode.com/posts")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+
+
+	// другой вариант POST запроса
+	// если передается map, то по умолчанию используется json
+	resp, err = client.R().
+		SetBody(map[string]interface{}{"title":"My title", "body":"Content", "userId": 7}).
+		Post("https://jsonplaceholder.typicode.com/posts")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+
+
+}
