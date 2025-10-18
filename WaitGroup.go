8f52fce07417f88962d7f1cdabd14f8949664adc
Usynin Sergei <usynin.s00@mail.ru>
2025-10-18 18:40:04 +0300

    Add halyva wait group.

---
+++ WaitGroup.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Printf("Worker %d is working\n", id)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
+	}
+	
+	wg.Wait()
+	fmt.Println("all workers are done\n")
+}
