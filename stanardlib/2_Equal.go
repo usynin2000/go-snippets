18dae806bc3365360218c431503c59d1e547eeef
Usynin Sergei <usynin.s00@mail.ru>
2025-10-21 18:16:13 +0300

    Add bytes.Equal

---
+++ stanardlib/2_Equal.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Equal reports whether a and b are the same length and contain the same bytes.
+func Equal(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a := []byte{'a', 'c'}
+	b := []byte{'a', 'c'}
+	c := []byte{'a', 'b', 'c'}
+
+	isEqual_a_b := Equal(a, b)
+	fmt.Println(isEqual_a_b)
+
+	isEqual_a_c := Equal(a, c)
+	fmt.Println(isEqual_a_c)
+
+
+
+}
