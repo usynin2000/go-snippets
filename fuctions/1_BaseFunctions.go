45c42c5e98d8584f7b600a9d29eef64d341dad6f
Usynin Sergei <usynin.s00@mail.ru>
2025-11-13 10:27:48 +0300

    functions.

---
+++ fuctions/1_BaseFunctions.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
+
+func divMod(a, b int) (int, int) {
+	return a / b, a % b
+}
+
+func main() {
+	fmt.Println("add: ", add(2, 3))
+	q, r := divMod(10, 3)
+	fmt.Println("div:", q, "mod:", r) 
+	// div: division result (quotient) 
+	// mod: remainder (modulo)
+}
