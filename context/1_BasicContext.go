8c363fe227bbfb382b728b59fb861be520e3ae16
Usynin Sergei <usynin.s00@mail.ru>
2025-11-30 23:28:24 +0300

    Add BasigContext.go

--- context/1_BasicContext.go
+++ context/1_BasicContext.go
@@ -13,4 +13,24 @@ func main() {
 	ctx := context.Background()
 	fmt.Printf("Background context: %v\n", ctx)
 
+	// TODO context - use when unsure which context to use
+	todoCtx := context.TODO()
+	fmt.Printf("TODO context: %v\n", todoCtx)
+
+	// Check if context is done (it's not)
+	select {
+	case <- ctx.Done():
+		fmt.Println("Context is done")
+	default:
+		fmt.Println("Context is still active")
+	}
+
+	// Context values
+	fmt.Printf("Context error: %v\n", ctx.Err())
+	
+	deadline, _ := ctx.Deadline()
+	fmt.Printf("Context deadline: %v\n", deadline)
+
+
+
 }
