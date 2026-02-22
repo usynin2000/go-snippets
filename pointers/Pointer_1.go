ae6c3463e06ce7e546eb0da4e035059f8b9e324a
Usynin Sergei <usynin.s00@mail.ru>
2026-02-22 20:49:16 +0300

    started pointers

--- pointers/Pointer_1.go
+++ pointers/Pointer_1.go
@@ -3,26 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// Обычная переменная
+	// Simple var
 	x := 10
 
-	fmt.Println("Значение x:", x)
+	fmt.Println("Value x:", x)
 
-	// Получаем указатель на переменную
+	// Getting pointer to the var x
 	ptr := &x
-	fmt.Println("Адрес x:", ptr)
-	fmt.Println("Значение через указатель", *ptr)
+	fmt.Println("Address x:", ptr)
+	fmt.Println("Value by using pointer", *ptr)
 
 	// Меняем значение через указатель
+	// Change the value by using pointer
 	*ptr = 20
-	fmt.Println("Новое значение x:", x)
+	fmt.Println("New value x:", x)
 
 	// Передача указателя в функцию
+	// Passing pointer to func
 	changeValue(ptr)
-	fmt.Println("Значение x после функции:", x)
+	fmt.Println("Value of x after func:", x)
 }
 
 // Функция, которая изменяет значение переменной по указателю
+// Func that change vlue of var by using pointer
 func changeValue(p *int) {
 	*p = *p + 5
 }
