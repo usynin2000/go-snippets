91c79d5e9736966ba6daff22f9942cc0266208c2
Usynin Sergei <usynin.s00@mail.ru>
2026-03-04 20:47:51 +0300

    Add something new to context

--- context/5_WithValue.go
+++ context/5_WithValue.go
@@ -11,11 +11,67 @@ import (
 type contextKey string
 
 const (
-	userIDKey	contextKey = "userID"
-	requestIDKey	contextKey = "requesID"
-	roleKey		contextKey = "role"
+	userIDKey    contextKey = "userID"
+	requestIDKey contextKey = "requesID"
+	roleKey      contextKey = "role"
 )
 
 func authenticate(ctx context.Context) context.Context {
-	// add user information to context
+	return context.WithValue(ctx, userIDKey, "user-123")
 }
+
+func setRequestID(ctx context.Context) context.Context {
+	return context.WithValue(ctx, requestIDKey, "req-abs-def")
+}
+
+func setRole(ctx context.Context, role string) context.Context {
+	return context.WithValue(ctx, roleKey, role)
+}
+
+
+// Helper functions - is good to extract values
+func getUserID(ctx context.Context) (string, bool) {
+	v  := ctx.Value(userIDKey)
+	if v == nil {
+		return "", false
+	}
+
+	id, ok := v.(string)
+	return id, ok
+}
+
+func getRequestID(ctx context.Context) string {
+	v := ctx.Value(requestIDKey)
+	if v == nil {
+		return ""
+	}
+	return v.(string)
+}
+
+func handler(ctx context.Context) {
+	userID, ok := getUserID(ctx)
+	if !ok {
+		fmt.Println("User not authenticated")
+		return
+	}
+
+	fmt.Printf(
+		"Processing request for user: %s, requestID: %s\nwith role %s",
+		userID, getRequestID(ctx), ctx.Value(roleKey),
+	)
+}
+
+
+func main() {
+	// emulate middleware
+
+	ctx := context.Background()
+	ctx = setRequestID(ctx)
+	ctx = authenticate(ctx)
+	ctx = setRole(ctx, "admin")
+
+	handler(ctx)
+}
+
+
+
